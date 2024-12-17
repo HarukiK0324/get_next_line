4ded061967abe5cb724db8c4c47819fd32dca27e
Haruki Kasamatsu <haruki@Harukis-MacBook-Pro.local>
2024-12-17 11:28:02 +0900

    modified all

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: haruki <haruki@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/10 12:53:20 by haruki            #+#    #+#             */
-/*   Updated: 2024/12/17 11:07:08 by haruki           ###   ########.fr       */
+/*   Updated: 2024/12/17 11:27:29 by haruki           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,27 +54,6 @@ int	find_newline(char *line)
 	return (-1);
 }
 
-char	*last_line(char *line)
-{
-	int		i;
-	char	*last_line;
-
-	i = 0;
-	while (line[i] != '\0')
-		i++;
-	last_line = (char *)malloc(sizeof(char) * (i + 1));
-	if (last_line == NULL)
-		return (NULL);
-	i = 0;
-	while (line[i] != '\0')
-	{
-		last_line[i] = line[i];
-		i++;
-	}
-	last_line[i] = '\0';
-	return (last_line);
-}
-
 char	*get_first_line(char *line)
 {
 	int		i;
@@ -84,7 +63,7 @@ char	*get_first_line(char *line)
 	while (line[i] != '\0' && line[i] != '\n')
 		i++;
 	if (line[i] == '\0' || (line[i] == '\n' && line[i + 1] == '\0'))
-		return line;
+		return (line);
 	first_line = (char *)malloc(sizeof(char) * (i + 2));
 	if (first_line == NULL)
 		return (NULL);
@@ -99,27 +78,27 @@ char	*get_first_line(char *line)
 	return (first_line);
 }
 
-char *update_line(char *line)
+char	*update_line(char *line)
 {
-	char *new_line;
-	int i;
-	int j;
+	char	*new_line;
+	int		i;
+	int		j;
 
 	i = 0;
 	j = 0;
-	while(line[i] != '\n')
+	while (line[i] != '\n')
 		i++;
 	new_line = (char *)malloc(ft_strlen(line) - i);
-	if(new_line == NULL)
-		return NULL;
-	while(line[i] != '\0')
+	if (new_line == NULL)
+		return (NULL);
+	while (line[i] != '\0')
 	{
 		new_line[j] = line[i];
 		i++;
 	}
 	new_line[j] = '\0';
 	free(line);
-	return new_line;
+	return (new_line);
 }
 
 char	*get_next_line(int fd)
@@ -138,7 +117,7 @@ char	*get_next_line(int fd)
 			break ;
 	}
 	first_line = get_first_line(line);
-	if(ft_strlen(first_line) == ft_strlen(line))
+	if (ft_strlen(first_line) == ft_strlen(line))
 	{
 		free(line);
 		line = NULL;
@@ -146,14 +125,4 @@ char	*get_next_line(int fd)
 	else
 		line = update_line(line);
 	return (first_line);
-}
-#include <stdio.h>
-
-int	main(void)
-{
-	char	*s;
-
-	s = get_next_line(1);
-	printf("%s\n", s);
-	return (0);
 }
