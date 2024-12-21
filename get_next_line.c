4d39b6c969b3d77d3286bb18f5101905f337a8dd
haruki <71203445+HarukiK0324@users.noreply.github.com>
2024-12-21 20:01:43 +0900

    added all

--- get_next_line.c
+++ get_next_line.c
@@ -6,40 +6,30 @@
 /*   By: haruki <haruki@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/10 12:53:20 by haruki            #+#    #+#             */
-/*   Updated: 2024/12/21 18:23:42 by haruki           ###   ########.fr       */
+/*   Updated: 2024/12/21 19:46:49 by haruki           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*get_line_from_buffer(char *line, int *fd)
+int ft_strlen(char *s)
 {
-	char	*buffer;
-	int		bytes_read;
-
-	buffer = malloc(BUFFER_SIZE + 1);
-	if (buffer == NULL)
-		return (NULL);
-	bytes_read = read(*fd, buffer, BUFFER_SIZE);
-	if (bytes_read <= 0)
-	{
-		free(buffer);
-		if (bytes_read == 0)
-			*fd = -1;
-		return (line);
-	}
-	buffer[bytes_read] = '\0';
-	line = ft_strjoin(line, buffer);
-	free(buffer);
-	return (line);
-}
-
-int	find_newline(char *line)
-{
-	int	i;
+	int i;
 
 	i = 0;
-	if (line == NULL)
+	if(s == NULL)
+		return (0);
+	while(s[i] != '\0')
+		i++;
+	return i;
+}
+
+int find_newline(char *line)
+{
+	int i;
+
+	i  = 0;
+	if(line == NULL)
 		return (-1);
 	while (line[i] != '\0')
 	{
@@ -50,77 +40,92 @@ int	find_newline(char *line)
 	return (-1);
 }
 
-char	*get_first_line(char *line)
-{
-	int		i;
-	char	*first_line;
-
-	i = 0;
-	if (line == NULL || line[0] == '\0')
-		return (NULL);
-	while (line[i] != '\0' && line[i] != '\n')
-		i++;
-	if (line[i] == '\n')
-		i++;
-	first_line = malloc(i + 1);
-	if (first_line == NULL)
-		return (NULL);
-	i = 0;
-	while (line[i] != '\0' && line[i] != '\n')
-	{
-		first_line[i] = line[i];
-		i++;
-	}
-	if (line[i] == '\n')
-		first_line[i++] = '\n';
-	first_line[i] = '\0';
-	return (first_line);
-}
-
-char	*update_line(char *line)
-{
-	int		i;
-	int		j;
-	char	*new_line;
-
-	i = 0;
-	j = 0;
-	if (line == NULL || line[0] == '\0')
-		return (NULL);
-	while (line[i] != '\0' && line[i] != '\n')
-		i++;
-	if (line[i] == '\0')
-	{
-		free(line);
-		return (NULL);
-	}
-	i++;
-	new_line = malloc(ft_strlen(line) - i + 1);
-	if (new_line == NULL)
-		return (NULL);
-	while (line[i])
-		new_line[j++] = line[i++];
-	new_line[j] = '\0';
-	free(line);
-	return (new_line);
-}
-
 char	*get_next_line(int fd)
 {
 	static char	*line;
-	char		*first_line;
+	char *buffer;
+	int result;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
 	while (find_newline(line) == -1)
 	{
-		line = get_line_from_buffer(line, &fd);
-		if (line == NULL)
+		buffer = malloc(BUFFER_SIZE + 1);
+		if (buffer == NULL)
 			return (NULL);
-		if (fd == -1)
-			break ;
+		result = read(fd, buffer, BUFFER_SIZE);
+		if (result == -1)
+		{
+			free(buffer);
+			return (NULL);
+		}
+		else if (result == 0)
+		{
+			if (line == NULL || line[0] == '\0')
+			{
+				free(buffer);
+				return (NULL);
+			}
+			break;
+		}
+		buffer[result] = '\0';
+		char *tmp = malloc(ft_strlen(line) + ft_strlen(buffer) + 1);
+		if (tmp == NULL)
+		{
+			free(buffer);
+			return (NULL);
+		}
+		int i = 0, j = 0;
+		while (line != NULL && line[i] != '\0')
+		{
+			tmp[i] = line[i];
+			i++;
+		}
+		while (buffer[j] != '\0')
+		{
+			tmp[i] = buffer[j];
+			i++;
+			j++;
+		}
+		tmp[i] = '\0';
+		free(line);
+		free(buffer);
+		line = tmp;
 	}
-	first_line = get_first_line(line);
-	line = update_line(line);
-	return (first_line);
+	if (find_newline(line) == -1)
+	{
+		if (line == NULL || line[0] == '\0')
+			return (NULL);
+		return (line);
+	}
+	int newline_index = find_newline(line);
+	char *tmp = malloc(newline_index + 2);
+	if (tmp == NULL)
+		return (NULL);
+	int i = 0;
+	while (line[i] != '\n')
+	{
+		tmp[i] = line[i];
+		i++;
+	}
+	tmp[i] = '\n';
+	i++;
+	tmp[i] = '\0';
+	char *tmp2 = malloc(ft_strlen(line) - i + 1);
+	if (tmp2 == NULL)
+	{
+		free(tmp);
+		return (NULL);
+	}
+	int j = 0;
+	while (line[i] != '\0')
+	{
+		tmp2[j] = line[i];
+		i++;
+		j++;
+	}
+	tmp2[j] = '\0';
+	free(line);
+	line = tmp2;
+	return (tmp);
 }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,43 +6,8 @@
 /*   By: haruki <haruki@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/10 12:53:16 by haruki            #+#    #+#             */
-/*   Updated: 2024/12/21 18:24:28 by haruki           ###   ########.fr       */
+/*   Updated: 2024/12/21 18:57:25 by haruki           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-
-int	ft_strlen(char *s)
-{
-	int	len = 0;
-
-	if (s == NULL)
-		return (0);
-	while (s[len] != '\0')
-		len++;
-	return (len);
-}
-
-char	*ft_strjoin(char *s1, char *s2)
-{
-	char	*result;
-	int		i = 0, j = 0;
-
-	if (s1 == NULL && s2 == NULL)
-		return (NULL);
-	result = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
-	if (result == NULL)
-		return (NULL);
-	if (s1 != NULL)
-		while (s1[i] != '\0')
-		{
-			result[i] = s1[i];
-			i++;
-		}
-	if (s2 != NULL)
-		while (s2[j] != '\0')
-			result[i++] = s2[j++];
-	result[i] = '\0';
-	free(s1);
-	return (result);
-}
